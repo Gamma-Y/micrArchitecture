d286f781141ceb2a56b86c5abae6534ab6772385
Evgeny Smirnov <160397jo@mail.ru>
2022-06-01 22:18:57 +0500

    create docker

--- mini_service/src/main/java/ru/smirnov/miniservice/metrics/Metric.java
+++ mini_service/src/main/java/ru/smirnov/miniservice/metrics/Metric.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Metric {
-
-    @GetMapping("/health/RESPONSE")
+    @GetMapping("/health")
     public StatusDTO getStatusOk() {
         return new StatusDTO("OK");
     }
